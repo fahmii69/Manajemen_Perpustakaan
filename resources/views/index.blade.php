1183192231b814a9980d083ce94561dbed520098
Abdianggi <abdianggi@gmail.com>
2022-12-07 16:59:18 +0800

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,19 +7,8 @@
     <!-- Begin Page Content -->
 <div class="container-fluid">
     <i class="fas fa-clock">
-        {{-- <span class="h3 mb-4 text-gray-800">Data Siswa</span> --}}
+        <span class="h3 mb-4 text-gray-800">Data Siswa</span>
     </i>
-    <!-- DataTales Example -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <a href="" class="btn btn-success btn-icon-split tombol-tambah">
-                <span class="icon text-white-50">
-                    <i class="fas fa-user-plus"></i>
-                </span>
-                <span class="text">Tambah Data</span>
-            </a>
-        </div>
-
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="yajra-dataTable" width="100%" cellspacing="0">
