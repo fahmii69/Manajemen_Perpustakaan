70d84bb3e88d6aa0b63b4007e89c804828f49e20
fahmii69 <katsurito12@gmail.com>
2022-12-09 15:09:08 +0800

    fix author error

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -96,7 +96,7 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Utilities:</h6>
                         <a class="collapse-item" href="utilities-color.html">Data Buku</a>
-                        <a class="collapse-item" href="{{ route('kategori.index') }}">Kategori Buku</a>
+                        {{-- <a class="collapse-item" href="{{ route('kategori.index') }}">Kategori Buku</a> --}}
                     </div>
                 </div>
             </li>
